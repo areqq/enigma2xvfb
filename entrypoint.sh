40783dee43bb39640fa8cccec7226735f0383a76
Areq <arek@patyk.net>
2026-01-02 15:14:17 +0100

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
+set -x
 
 # required directory for enigma2
 mkdir -p /dev/input
 
-# start web server
-service apache2 start
-
 echo "start Xvfb"
-test -z "$RESOLUTION" && RESOLUTION="1920x1080x16"
-Xvfb "$DISPLAY" -ac -screen 0 "$RESOLUTION" &
+#Xvfb :99 -screen 0 1920x1080x24 -ac -nolisten tcp -noreset &
+tigervncpasswd -f <<< 'q' > /root/passwd
+chmod 600 /root/passwd
+Xvnc :99 -rfbport 5900 -geometry 1920x1080 -depth 24 -Protocol3.3 -localhost no -SecurityTypes VncAuth -PasswordFile /root/passwd
 xvfb_pid=$!
 echo "exec command $@"
 exec "$@"
